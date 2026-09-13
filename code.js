578dc994f2aa3bd1bf9f29cb5e7ca1f8ce26b8ff
Eugene Alvin Villar <seav80@gmail.com>
2026-09-13 15:51:44 +0800

    Split out JS

---
+++ code.js
@@ -0,0 +1,236 @@
+'use strict';
+
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7/+esm';
+
+// ----------------------------------------
+// FUNDAMENTAL CONSTANTS
+
+const TWO_PI = Math.PI * 2;
+const SECS_IN_HOUR = 60 * 60;
+const SECS_IN_12_HOURS = SECS_IN_HOUR * 12;
+const PH_TZ_OFFSET = 8;  // in hours
+
+// ----------------------------------------
+// APP PARAMETERS
+
+const MIN_Y = 0.5;
+const MIN_TIMESTAMP = 1284163200;  // 8am
+const MAX_TIMESTAMP = 1284202800;  // 7pm
+
+const X_SCALE = 256;
+const Y_SCALE = 116;
+const X_TRIM = -0.75;
+const Y_TRIM = 1.08;
+
+const TIMESTAMP_DELTA = 20;
+
+const MAP_WIDTH = 1200;
+const MAP_HEIGHT = 900;
+const CANVAS_WIDTH = 1600;
+const CANVAS_HEIGHT = 1200;
+
+const CLOCK_RADIUS = 150;
+const CLOCK_HAND_COLOR = '#ffc';
+const CLOCK_FACE_COLOR = '#555';
+const CLOCK_TICK_COLOR = '#888';
+const CLOCK_MAJOR_TICK_WIDTH = 7;
+const CLOCK_MINOR_TICK_WIDTH = 2;
+const CLOCK_TICK_MIN_RADIUS = 100;
+const CLOCK_TICK_MAX_RADIUS = 140;
+const CLOCK_HOUR_HAND_WIDTH = 10;
+const CLOCK_MINUTE_HAND_WIDTH = 10;
+const CLOCK_HOUR_HAND_RADIUS = 80;
+const CLOCK_MINUTE_HAND_RADIUS = 140;
+const CLOCK_HAND_REVERSE_RADIUS = 30;
+
+const TRACK_WIDTH = 3;
+const RED_TEAM_COLOR = '#f66';
+const ORANGE_TEAM_COLOR = '#fb6';
+const GREEN_TEAM_COLOR = '#6f6';
+const TEAM_POS_RADIUS = 8;
+const TEAM_POS_COLOR = '#ff8';
+const TILE_X0 = 3419;
+const TILE_X1 = 3425;
+const TILE_Y0 = 1881;
+const TILE_Y1 = 1886;
+const TILE_SCALED_SIZE = 192;
+const TILE_X_ADJUST = -79;
+const TILE_Y_ADJUST = -138;
+
+// ----------------------------------------
+// GLOBAL VARIABLES
+
+const TrackCtx      = document.getElementById('track'     ).getContext('2d');
+const ForegroundCtx = document.getElementById('foreground').getContext('2d');
+
+let RedData, GreenData, OrangeData;
+let TimestampIter = MIN_TIMESTAMP;
+
+// References to DOM elements
+const MainDiv = d3.select('main');
+let HourHand, MinuteHand;
+
+TrackCtx.lineWidth = TRACK_WIDTH;
+TrackCtx.lineCap = 'round';
+TrackCtx.lineJoin = 'round';
+
+// ----------------------------------------
+// FUNCTIONS
+
+const getJson = filename => fetch(filename).then(response => response.json());
+
+const fitToViewport = () => {
+  const scale = Math.min(
+    window.innerWidth  / MAP_WIDTH,
+    window.innerHeight / MAP_HEIGHT,
+  );
+  MainDiv.node().style.transform = `scale(${scale})`;
+};
+
+const processRawData = (data) => {
+  const finalData = [];
+  for (const record of data) {
+    if (record[0] > MIN_TIMESTAMP && record[0] < MAX_TIMESTAMP && record[2] > MIN_Y) {
+      const x = (record[1] - X_TRIM) * X_SCALE;
+      const y = (record[2] - Y_TRIM) * Y_SCALE;
+      finalData.push([record[0], x, y]);
+    }
+  }
+  return finalData;
+};
+
+const drawBaseMap = () => {
+  const div = document.getElementById('base-map');
+  for (let x = TILE_X0; x <= TILE_X1; x++) {
+    for (let y = TILE_Y0; y <= TILE_Y1; y++) {
+      const tile = new Image();
+      tile.className = 'tile';
+      tile.src = `https://basemaps.cartocdn.com/rastertiles/dark_all/12/${x}/${y}@2x.png?key=cb1_2ive_2_49fd2dc5752eb5e5a73c9e7c`;
+      tile.style.left = `${(x - TILE_X0) * TILE_SCALED_SIZE + TILE_X_ADJUST}px`;
+      tile.style.top  = `${(y - TILE_Y0) * TILE_SCALED_SIZE + TILE_Y_ADJUST}px`;
+      div.appendChild(tile);
+    }
+  }
+};
+
+const createClock = () => {
+
+  const clockSvg = d3.select('#clock');
+  clockSvg
+    .attr('viewBox', `${-CLOCK_RADIUS} ${-CLOCK_RADIUS} ${CLOCK_RADIUS * 2} ${CLOCK_RADIUS * 2}`);
+
+  // Clock background
+  clockSvg.append('circle')
+    .attr('r', CLOCK_RADIUS)
+    .attr('fill', CLOCK_FACE_COLOR)
+    .attr('stroke', 'none');
+
+  // Clock tick marks
+  for (let hour = 0; hour < 12; hour++) {
+    clockSvg.append('path')
+      .attr('d', `M0,${CLOCK_TICK_MIN_RADIUS}L0,${CLOCK_TICK_MAX_RADIUS}`)
+      .attr('fill', 'none')
+      .attr('stroke', CLOCK_TICK_COLOR)
+      .attr('stroke-width', hour % 3 === 0 ? CLOCK_MAJOR_TICK_WIDTH : CLOCK_MINOR_TICK_WIDTH)
+      .attr('transform', `rotate(${360 * hour / 12})`)
+  }
+
+  // Clock hour hand
+  HourHand = clockSvg.append('path')
+    .attr('d', `M0,${CLOCK_HAND_REVERSE_RADIUS}L${-CLOCK_HOUR_HAND_WIDTH / 2},0L0,${-CLOCK_HOUR_HAND_RADIUS}L${CLOCK_HOUR_HAND_WIDTH / 2},0Z`)
+    .attr('fill', CLOCK_HAND_COLOR)
+    .attr('stroke', 'none');
+
+  // Clock minute hand
+  MinuteHand = clockSvg.append('path')
+    .attr('d', `M0,${CLOCK_HAND_REVERSE_RADIUS}L${-CLOCK_MINUTE_HAND_WIDTH / 2},0L0,${-CLOCK_MINUTE_HAND_RADIUS}L${CLOCK_MINUTE_HAND_WIDTH / 2},0Z`)
+    .attr('fill', CLOCK_HAND_COLOR)
+    .attr('stroke', 'none');
+};
+
+const updateClock = (timestamp) => {
+
+  const timeDelta = timestamp - (MIN_TIMESTAMP - PH_TZ_OFFSET * SECS_IN_HOUR);
+
+  const hourAngle = timeDelta / SECS_IN_12_HOURS * 360;
+  HourHand.attr('transform', `rotate(${hourAngle})`);
+
+  const minuteAngle = (timeDelta % SECS_IN_HOUR) / SECS_IN_HOUR * 360;
+  MinuteHand.attr('transform', `rotate(${minuteAngle})`);
+};
+
+const drawGpx = (data, strokeStyle, maxTime) => {
+
+  let hasStarted;
+  let firstRecord = data[0];
+  let lastRecord;
+
+  // Draw portion of track between maxTime - TIMESTAMP_DELTA and maxTime
+  for (const record of data) {
+    if (record[0] < maxTime - TIMESTAMP_DELTA) {
+      firstRecord = record;
+    }
+    else if (record[0] <= maxTime) {
+      if (!hasStarted) {
+        hasStarted = true;
+        TrackCtx.beginPath();
+        TrackCtx.moveTo(firstRecord[1], firstRecord[2]);
+      }
+      TrackCtx.lineTo(record[1], record[2]);
+
+      lastRecord = record;
+    }
+    else break;
+  }
+  if (hasStarted) {
+    TrackCtx.strokeStyle = strokeStyle;
+    TrackCtx.stroke();
+  }
+  else {
+    lastRecord = firstRecord;
+  }
+
+  // Draw current position of team as a circle
+  ForegroundCtx.fillStyle = TEAM_POS_COLOR;
+  ForegroundCtx.beginPath();
+  ForegroundCtx.ellipse(lastRecord[1], lastRecord[2], TEAM_POS_RADIUS, TEAM_POS_RADIUS, 0, 0, TWO_PI);
+  ForegroundCtx.fill();
+};
+
+const drawFrame = () => {
+
+  ForegroundCtx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+  updateClock(TimestampIter);
+
+  drawGpx(RedData   , RED_TEAM_COLOR   , TimestampIter);
+  drawGpx(OrangeData, ORANGE_TEAM_COLOR, TimestampIter);
+  drawGpx(GreenData , GREEN_TEAM_COLOR , TimestampIter);
+
+  if (TimestampIter < MAX_TIMESTAMP) {
+    TimestampIter += TIMESTAMP_DELTA;
+    requestAnimationFrame(drawFrame);
+  }
+}
+
+// ----------------------------------------
+// MAIN PROCESS
+
+window.addEventListener('resize', fitToViewport);
+window.addEventListener('orientationchange', fitToViewport);
+
+fitToViewport();
+drawBaseMap();
+createClock();
+updateClock(MIN_TIMESTAMP);
+
+setTimeout(
+  () => {
+    Promise.all([
+      getJson('red.json'   ).then(data => { RedData    = processRawData(data) }),
+      getJson('green.json' ).then(data => { GreenData  = processRawData(data) }),
+      getJson('orange.json').then(data => { OrangeData = processRawData(data) }),
+    ]).then(() => { requestAnimationFrame(drawFrame) });
+  },
+  1000,
+);
